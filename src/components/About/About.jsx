1aa2ca0e25ec46217b5cdfde0ad9e8804e1cdda9
Stacey Graham <stacey.graham1106@gmail.com>
2020-12-08 14:46:18 -0500

    First commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    'I fell in love with technology in the 7th grade in my first computer class and from there my curiosity for technology began. I am passionate about coding and I love learning and problem-solving collaboratively in teams.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    'Being able to take a step back and really understand the problem I am looking to solve is really important. I enjoy building high quality applications in JavaScript and React and working with RESTful APIs, Node.js, and Express.js. I also enjoy using databases like PostgreSQL to store data. I like being challenged and there is always an opportunity to learn something new in web development and that is what gets me excited about this industry.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || 'My favorite things to do in my spare time is yoga, gardening, creating beaded jewelry, traveling and spending time with my friends & family.'}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/mock/data.js
@@ -0,0 +1,89 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Stacey Graham',
+  subtitle: 'A Software Engineer in Atlanta, GA',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'ckprofilepic.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://bit.ly/37N8x26', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'Tunebuzz_Homepage.jpg',
+    title: 'Tunebuzz',
+    info: 'An app that allows a user to create a new awesome playlist after selecting 3 music genres.  Once the user selects their genres they will see a new playlist displayed with 15 new songs to dance to.',
+    info2: '- Built using HTML, CSS, JavaScript,Node.js, Express.js, Passport.js, PostgreSQL, Sequelize, and Spotify API.',
+    url: 'https://dry-beach-63193.herokuapp.com/',
+    repo: 'https://github.com/StashaG/tunebuzz', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Pantry chef 1 of 9.jpg',
+    title: 'Pantry Chef',
+    info: 'A recipe app that allows users to search for meal recipes using the ingredients they have on hand.',
+    info2: 'Built using HTML, CSS, JavaScript, Bootstrap, jQuery UI, Spoonacular API, and MealDB API',
+    url: 'https://pantry-chef.surge.sh/',
+    repo: 'https://github.com/StashaG/final-phase-1', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Success_Quote.jpg',
+    title: 'Random Quote Generator',
+    info: 'An app that displays random famous quotes each time a button is clicked. This app utilizes DOM manipulation and arrays.',
+    info2: 'Built using HTML, CSS, and JavaScript.',
+    url: 'https://stashag.github.io/Random-Quote-Generator/',
+    repo: 'https://github.com/StashaG/Random-Quote-Generator', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'stacey.graham1106@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: 'https://twitter.com/staceyluvstech',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/staceygraham1106/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'http://github.com/stashag',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
