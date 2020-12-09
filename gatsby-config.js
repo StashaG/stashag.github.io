2dfbb02965d835c3c34b40eaede35bf70084838e
Stacey Graham <stacey.graham1106@gmail.com>
2020-12-08 23:07:47 -0500

    updating title page

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Stacey Graham`,
-        short_name: `stacey.codes`,
+        name: `Gatsby Simplefolio`,
+        short_name: `Simplefolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
